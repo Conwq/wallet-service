db08255fb74d810d5f3552333d104eef15d61bc3
Сергей Пацеев <serega.patseev@gmail.com>
2023-10-25 01:25:40 +0300

    refactoring LoggerServiceTest

--- src/main/java/org/example/walletservice/model/entity/Log.java
+++ src/main/java/org/example/walletservice/model/entity/Log.java
@@ -1,5 +1,7 @@
 package org.example.walletservice.model.entity;
 
+import java.util.Objects;
+
 /**
  * A data class representing a log entry.
  * This class is annotated with Lombok annotations for generating boilerplate code.
@@ -34,5 +36,27 @@ public class Log {
 
 	public void setPlayerID(int playerID) {
 		this.playerID = playerID;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Log log1 = (Log) o;
+		return logID == log1.logID && playerID == log1.playerID && Objects.equals(log, log1.log);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(logID, log, playerID);
+	}
+
+	@Override
+	public String toString() {
+		return "Log{" +
+				"logID=" + logID +
+				", log='" + log + '\'' +
+				", playerID=" + playerID +
+				'}';
 	}
 }
